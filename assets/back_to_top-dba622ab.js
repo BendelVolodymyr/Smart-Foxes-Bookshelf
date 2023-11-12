304f719617de90f756fafa9c8a5a7e830a04fffb
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2023-11-12 10:52:41 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-Bookshelf@319b0e4c0b03a49d0df12e1e05b26c2801730056 🚀

---
+++ assets/back_to_top-dba622ab.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".back-to-top");o.addEventListener("click",t);window.onscroll=()=>{e()};function t(){window.scrollTo({top:0,behavior:"smooth"})}function e(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.classList.remove("is-hidden"):o.classList.add("is-hidden")}
+//# sourceMappingURL=back_to_top-dba622ab.js.map
