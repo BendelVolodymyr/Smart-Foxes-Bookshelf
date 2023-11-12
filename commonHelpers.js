80e8b9bc642ed77e79adad7ef9fe024d1a12bdb8
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2023-11-12 11:22:02 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-Bookshelf@9066c4e370cf9d1dbb662ccdd8a61a2c24641246 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,29 +1,31 @@
-import"./assets/toggle_theme-e463d6d1.js";import{a as n}from"./assets/vendor-1c5e86dd.js";import"./assets/back_to_top-dba622ab.js";const i="https://books-backend.p.goit.global/books/";async function l(){const e="category-list";return await n.get(`${i}${e}`)}async function g(){const e="top-books";return await n.get(`${i}${e}`)}async function d(e){const o="category?category=";return await n.get(`${i}${o}${e}`)}const c=document.querySelector(".categories-books-all"),u=document.querySelector(".categories-books-title"),k=document.querySelector(".loader");window.addEventListener("load",b);c.addEventListener("click",p);async function b(){k.style.display="none";try{const e=await g(),o=m(e.data);$(o)}catch(e){console.log(e)}}function m(e=[]){return e.map(({list_name:o,books:t})=>`
+import"./assets/toggle_theme-445b1a1a.js";import{a as i}from"./assets/vendor-1c5e86dd.js";import"./assets/back_to_top-dba622ab.js";const l="https://books-backend.p.goit.global/books/";async function u(){const e="category-list";return await i.get(`${l}${e}`)}async function g(){const e="top-books";return await i.get(`${l}${e}`)}async function y(e){const o="category?category=";return await i.get(`${l}${o}${e}`)}const m=document.querySelector(".categories-books"),a=document.querySelector(".categories-books-all"),c=document.querySelector(".categories-books-title"),k=document.querySelector(".loader");window.addEventListener("load",p);a.addEventListener("click",v);m.addEventListener("click",B);a.addEventListener("click",C);a.addEventListener("click",E);async function p(){k.style.display="none";try{const e=await g(),o=d(e.data);h(o)}catch(e){console.log(e)}}async function f(){k.style.display="none";try{const e=await g(),o=d(e.data);a.innerHTML=o}catch(e){console.log(e)}}function B(e){if(!e.target.classList.contains("categories-books-back-btn"))return;const o=e.target;f(),o.remove(),c.innerHTML='Best Sellers <span class="categories-books-title-accent">Books'}function d(e=[]){return e.map(({list_name:o,books:s})=>`
       <div class="categories-books-genre-title"> ${o}
         <ul class="categories-books-genre-all-books">
-        ${y(t)}
+        ${$(s)}
         </ul>
         <button class="categories-books-btn">see more</button>
       </div>
-      `).join("")}function y(e=[]){return e.map(({_id:o,list_name:t,author:s,book_image:a,title:r})=>`
-      <li class="categories-books-genre-book" data-id="${o}" data-genre="${t}">
+      `).join("")}function $(e=[]){return e.map(({_id:o,list_name:s,author:r,book_image:n,title:t})=>`
+      <li class="categories-books-genre-book" data-id="${o}" data-genre="${s}">
         <div class="categories-books-genre-book-img-thumb">
           <img class="categories-books-genre-book-img"
-            src="${a}" alt="Title - '${r}'">
+            src="${n}" alt="Title - '${t}'">
+          <div class="categories-books-genre-book-hover">quick view </div>
         </div>
-        <h2 class="categories-books-genre-book-title">${r}</h2>
-        <h3 class="categories-books-genre-book-author">${s}</h3>
+        <h2 class="categories-books-genre-book-title">${t}</h2>
+        <h3 class="categories-books-genre-book-author">${r}</h3>
       </li>
-      `).join("")}function $(e){c.insertAdjacentHTML("beforeend",e)}async function p(e){if(!e.target.classList.contains("categories-books-btn"))return;const t=e.target.previousElementSibling.firstElementChild.dataset.genre;u.innerHTML=t;try{const s=await d(t),a=h(s.data);f(a)}catch(s){console.log(s)}}function h(e=[]){return e.map(({_id:o,list_name:t,author:s,book_image:a,title:r})=>`
+      `).join("")}function h(e){a.insertAdjacentHTML("beforeend",e)}async function v(e){if(!e.target.classList.contains("categories-books-btn"))return;const s=e.target.previousElementSibling.firstElementChild.dataset.genre,r=s.split(" "),n=r.splice(-1,1);c.innerHTML=`${r.join(" ")} <span class="categories-books-title-accent">${n.toString()}</span>`,c.insertAdjacentHTML("afterend",'<button class="categories-books-back-btn">back to Best Sellers</button>');try{const t=await y(s),b=L(t.data);S(b)}catch(t){console.log(t)}}function L(e=[]){return e.map(({_id:o,list_name:s,author:r,book_image:n,title:t})=>`
       <div class="categories-books-genre-all-books">
-        <div class="categories-books-genre-books" data-id="${o}" data-genre="${t}">
+        <div class="categories-books-genre-books" data-id="${o}" data-genre="${s}">
           <div class="categories-books-genre-book-img-thumb">
             <img class="categories-books-genre-book-img"
-              src="${a}" alt="Title - '${r}'">
+              src="${n}" alt="Title - '${t}'">
+              <div class="categories-books-genre-book-hover">quick view </div>
           </div>
-          <h2 class="categories-books-genre-book-title">${r}</h2>
-          <h3 class="categories-books-genre-book-author">${s}</h3>
+          <h2 class="categories-books-genre-book-title">${t}</h2>
+          <h3 class="categories-books-genre-book-author">${r}</h3>
         </div>
       </div> 
-      `).join("")}function f(e){c.innerHTML=e}const B={allCategoriesContainer:document.querySelector(".all-categories-container"),allCategoriesList:document.querySelector(".js-categories-list"),allCategoriesListItem:document.querySelector(".js-categories-list-item"),categoriesBook:document.querySelector(".categories-books-all"),categoriesBooksTitle:document.querySelector(".categories-books-title")};function C(e){return e.map(({list_name:o})=>`<li class="categories-list-item js-categories-list-item" data-category='${o}'><a href="">${o}</a></li>`).join("")}l().then(e=>{B.allCategoriesList.insertAdjacentHTML("beforeend",C(e.data))}).catch(e=>console.log(e));
+      `).join("")}function S(e){a.innerHTML=e}async function C(e){if(!e.target.closest(".categories-books-genre-book"))return;e.target.closest(".categories-books-genre-book").dataset.id}async function E(e){if(!e.target.closest(".categories-books-genre-books"))return;e.target.closest(".categories-books-genre-books").dataset.id}const q={allCategoriesContainer:document.querySelector(".all-categories-container"),allCategoriesList:document.querySelector(".js-categories-list"),allCategoriesListItem:document.querySelector(".js-categories-list-item"),categoriesBook:document.querySelector(".categories-books-all"),categoriesBooksTitle:document.querySelector(".categories-books-title")};function M(e){return e.map(({list_name:o})=>`<li class="categories-list-item js-categories-list-item" data-category='${o}'><a href="">${o}</a></li>`).join("")}u().then(e=>{q.allCategoriesList.insertAdjacentHTML("beforeend",M(e.data))}).catch(e=>console.log(e));document.querySelector(".suport");document.querySelector(".suport__button");
 //# sourceMappingURL=commonHelpers.js.map
