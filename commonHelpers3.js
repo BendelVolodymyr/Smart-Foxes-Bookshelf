6926a46f7722bc4c4e40f3b5498e49e7f882afb8
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2023-11-13 20:26:24 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-Bookshelf@0fd369aacef43cb8737e62995981295f020917fe 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/toggle_theme-88cb29af.js";import"./assets/back_to_top-62c9c55b.js";const i=localStorage.getItem("shoppingList"),o=JSON.parse(i);console.log(o);function n(t){t.map(({_id:s,amazon_product_url:e,author:a,book_image:p,list_name:l,title:r,buy_links:c})=>{})}n(o);
+import"./assets/toggle_theme-ccdc2c6d.js";import"./assets/back_to_top-62c9c55b.js";const i=localStorage.getItem("shoppingList"),o=JSON.parse(i);console.log(o);function n(t){t.map(({_id:s,amazon_product_url:e,author:a,book_image:p,list_name:l,title:r,buy_links:c})=>{})}n(o);
 //# sourceMappingURL=commonHelpers3.js.map
