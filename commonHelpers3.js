e02aaffb1ee22d1e7f09c8e3b72b7dae5dab81d6
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2023-11-13 22:18:29 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-Bookshelf@339f8a72a2074abed5089c1255d1ecc4173dc3a1 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,4 +1,4 @@
-import"./assets/toggle_theme-056aedf1.js";import"./assets/back_to_top-62c9c55b.js";const c=localStorage.getItem("shoppingList"),h=JSON.parse(c),t=document.querySelector(".shopping-default"),r=document.querySelector(".shopping-title");function m(s){let o=[];s.map(({description:i,amazon_product_url:n,author:e,book_image:a,list_name:p,title:l,buy_links:g})=>{o.push(`<div class="shopping-box">
+import"./assets/toggle_theme-056aedf1.js";import"./assets/back_to_top-62c9c55b.js";const c=localStorage.getItem("shoppingList"),h=JSON.parse(c),i=document.querySelector(".shopping-default"),r=document.querySelector(".shopping-title");function m(s){let o=[];s.map(({description:t,amazon_product_url:n,author:e,book_image:a,list_name:p,title:l,buy_links:g})=>{o.push(`<div class="shopping-box">
         <div class="shopping-book-img">
           <img class="shopping-genre-img" src="${a}"
             alt="Title - 'WISH'" >
@@ -6,7 +6,7 @@ import"./assets/toggle_theme-056aedf1.js";import"./assets/back_to_top-62c9c55b.j
         <div class="shopping-book-info">
           <h2 class="shopping-book-title">${l}</h2>
           <span class="shopping-book-category">${p}</span>
-          <p class="shopping-book-discription">${i}</p>
+          <p class="shopping-book-discription">${t}</p>
           <span class="shopping-book-author">${e}</span>
           <a class="img__src-amazon" href="${n}"><img class="mdl-logo-amazon" src="../img/amazon.png" alt="logo amazon" width="62" height="19"></a>
           <a class="img__src-books" href="${g[1].url}"><img class="mdl-logo-book" src="../img/books.png" alt="logo book" width="33" height="32"></a>
@@ -16,5 +16,5 @@ import"./assets/toggle_theme-056aedf1.js";import"./assets/back_to_top-62c9c55b.j
             <use href="../img/sprite.svg#icon-trash"></use>
           </svg>
         </button>
-      </div>`)}),s.length==[].length?t.classList.remove("is-hidden"):(t.classList.add("is-hidden"),r.insertAdjacentHTML("afterend",o))}m(h);
+      </div>`)}),s.length==[].length?i.classList.remove("is-hidden"):(i.classList.add("is-hidden"),r.insertAdjacentHTML("afterend",o.join("")))}m(h);
 //# sourceMappingURL=commonHelpers3.js.map
